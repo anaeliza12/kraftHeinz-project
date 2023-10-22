9103c1e9184b560005fb432cab5791541045a73c
Ana Eliza <anaelizaperobelli@gmail.com>
2023-10-22 20:22:59 -0300

    LoginService creation

---
+++ src/main/java/com/kraftHeinz/demo/service/LoginService.java
@@ -0,0 +1,46 @@
+package com.kraftHeinz.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kraftHeinz.demo.entities.Login;
+import com.kraftHeinz.demo.repository.LoginRepository;
+
+@Service
+public class LoginService {
+
+	@Autowired
+	private LoginRepository repository;
+
+	public boolean authentication(Login login) {
+		List<Login> logins = repository.findAll();
+		Boolean verify = false;
+		for (Login l : logins) {
+			if (login.getUsername().equals(l.getUsername()) && login.getPassword().equals(l.getPassword())) {
+				verify = true;
+				break;
+			}
+		}
+
+		return verify;
+
+	}
+	
+	public boolean recoverPassword(String email, String password) {
+		boolean verify = false;
+		List<Login> logins = repository.findAll();
+		for (Login l : logins) {
+			if (l.getUsername().equalsIgnoreCase(email)) {
+				l.setPassword(password);
+				verify = true;
+				break;
+			}
+
+		}
+		return verify;
+
+	}
+
+}
