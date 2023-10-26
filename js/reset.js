5c2252687e29b35c0ae272458de0b7ec8486b1f9
Ana Eliza <anaelizaperobelli@gmail.com>
2023-10-25 21:44:17 -0300

    ResetPassword front-end creation

---
+++ js/reset.js
@@ -0,0 +1,91 @@
+const dataEmail = document.getElementById("email")
+const passwordField = document.getElementById("password");
+
+
+let email = null;
+
+let passw = null;
+
+const pass = async () => {
+
+    const options = {
+        method: "PUT",
+        body: passw,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    const url = "http://localhost:8080/reset-password"
+    try {
+
+        const connection = await fetch(url, options)
+        const response = connection.status
+
+        if (response == 200) {
+            alert("Password changed successfully")
+
+        } else if (response == 404) {
+            alert("Login failed. Please check your username and password.")
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
+
+}
+const reset = async () => {
+
+    const options = {
+        method: "POST",
+        body: email,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    const url = "http://localhost:8080/reset-password"
+    try {
+
+        const connection = await fetch(url, options)
+        const response = connection.status
+
+        if (response == 200) {
+
+            passwordField.removeAttribute("hidden");
+
+
+        } else if (response == 404) {
+            alert("Login failed. Please check your username and password.")
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
+
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form")
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+
+        if (passwordField.hasAttribute("hidden")) {
+            // Se o campo de senha estiver oculto, chame a função `reset`
+            email = dataEmail.value;
+            reset();
+        } else {
+            // Se o campo de senha não estiver oculto, chame a função `pass`
+            passw = passwordField.value;
+            pass();
+        }
+
+    })
+
+
+
+})
